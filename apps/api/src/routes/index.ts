44057decf00198a1674a2e6a7deb6404a70aed39
CaptainZiboo <captainziboo@gmail.com>
2024-04-02 09:20:34 +0200

    feat: rust implementation + first project

--- apps/api/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import Router from "koa-router";
-import { authRouter } from "./auth.js";
-
-export const router = new Router();
-
-router.use("/auth", authRouter.routes());
--- apps/api/src/validations/register.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const RegisterSchema = z.object({
-  email: z.string().min(3).max(100),
-  password: z.string().min(6).max(100),
-});
